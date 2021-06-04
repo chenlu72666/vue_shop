664b1a61842e061c0c546a8450be9dc06b268e58
chenlu72666 <5146027+chenlu72666@user.noreply.gitee.com>
2021-06-04 22:37:42 +0800

    order

---
+++ src/components/order/Order.vue
@@ -0,0 +1,227 @@
+<template>
+  <div>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+      <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- card -->
+    <el-card>
+      <el-row>
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" v-model="queryInfo.query">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-col>
+      </el-row>
+      <!-- table -->
+      <el-table :data="orderList" border stripe>
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column label="订单编号" prop="order_number"></el-table-column>
+        <el-table-column label="订单价格" prop="order_price"></el-table-column>
+        <el-table-column label="是否付款" prop="pay_status">
+          <template slot-scope="scope">
+            <el-tag type="danger" v-if="scope.row.pay_status!=='1'">未付款</el-tag>
+            <el-tag type="success" v-else>已付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="是否发货" prop="is_send"></el-table-column>
+        <el-table-column label="下单时间" prop="create_time">
+          <template slot-scope="scope">{{scope.row.create_time|dateFormat}}</template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template>
+            <el-button type="primary" icon="el-icon-edit" size="mini" @click="showDialog"></el-button>
+            <el-button type="success" icon="el-icon-location" size="mini" @click="showProDia"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15,20]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
+    </el-card>
+    <!-- 修改地址对话框 -->
+    <el-dialog title="修改地址" :visible.sync="AddDialogVisible" width="50%" @close="resetForm">
+      <el-form
+        ref="addressFormRef"
+        :model="addressForm"
+        :rules="addressFormRules"
+        label-width="100px"
+      >
+        <el-form-item label="省市区/县" prop="address1">
+          <el-cascader
+            v-model="addressForm.address1"
+            :options="city"
+            :props="{ expandTrigger: 'hover' }"
+            @change="handleChange"
+          ></el-cascader>
+        </el-form-item>
+        <el-form-item label="详细地址" prop="address2">
+          <el-input v-model="addressForm.address2"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="AddDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="AddDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 物流进度对话框 -->
+    <el-dialog title="物流信息" :visible.sync="ProgressDialogVisible" width="50%">
+      <!-- 时间线 -->
+      <el-timeline :reverse="reverse">
+        <el-timeline-item
+          v-for="(activity, index) in progressInfo"
+          :key="index"
+          :timestamp="activity.time"
+        >{{activity.context}}</el-timeline-item>
+      </el-timeline>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import City from './citydata.js'
+export default {
+  data() {
+    return {
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      orderList: [],
+      total: 0,
+      AddDialogVisible: false,
+      addressForm: {
+        address1: '',
+        address2: ''
+      },
+      addressFormRules: {
+        address1: [
+          { required: true, message: '请选择省市区/县', trigger: 'blur' }
+        ],
+        address2: [
+          { required: true, message: '请选择详细地址', trigger: 'blur' }
+        ]
+      },
+      city: City,
+      ProgressDialogVisible: false,
+      progressInfo: [
+        {
+          time: '2018-05-10 09:39:00',
+          ftime: '2018-05-10 09:39:00',
+          context: '已签收,感谢使用顺丰,期待再次为您服务',
+          location: ''
+        },
+        {
+          time: '2018-05-10 08:23:00',
+          ftime: '2018-05-10 08:23:00',
+          context:
+            '[北京市]北京海淀育新小区营业点派件员 顺丰速运 95338正在为您派件',
+          location: ''
+        },
+        {
+          time: '2018-05-10 07:32:00',
+          ftime: '2018-05-10 07:32:00',
+          context: '快件到达 [北京海淀育新小区营业点]',
+          location: ''
+        },
+        {
+          time: '2018-05-10 02:03:00',
+          ftime: '2018-05-10 02:03:00',
+          context:
+            '快件在[北京顺义集散中心]已装车,准备发往 [北京海淀育新小区营业点]',
+          location: ''
+        },
+        {
+          time: '2018-05-09 23:05:00',
+          ftime: '2018-05-09 23:05:00',
+          context: '快件到达 [北京顺义集散中心]',
+          location: ''
+        },
+        {
+          time: '2018-05-09 21:21:00',
+          ftime: '2018-05-09 21:21:00',
+          context: '快件在[北京宝胜营业点]已装车,准备发往 [北京顺义集散中心]',
+          location: ''
+        },
+        {
+          time: '2018-05-09 13:07:00',
+          ftime: '2018-05-09 13:07:00',
+          context: '顺丰速运 已收取快件',
+          location: ''
+        },
+        {
+          time: '2018-05-09 12:25:03',
+          ftime: '2018-05-09 12:25:03',
+          context: '卖家发货',
+          location: ''
+        },
+        {
+          time: '2018-05-09 12:22:24',
+          ftime: '2018-05-09 12:22:24',
+          context: '您的订单将由HLA（北京海淀区清河中街店）门店安排发货。',
+          location: ''
+        },
+        {
+          time: '2018-05-08 21:36:04',
+          ftime: '2018-05-08 21:36:04',
+          context: '商品已经下单',
+          location: ''
+        }
+      ]
+    }
+  },
+  created() {
+    this.getOrderList()
+  },
+  methods: {
+    async getOrderList() {
+      const { data: res } = await this.$http.get('orders', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.orderList = res.data.goods
+      this.total = res.data.total
+      console.log(res)
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getOrderList()
+    },
+    handleCurrentChange(newNum) {
+      this.queryInfo.pagenum = newNum
+      this.getOrderList()
+    },
+    showDialog() {
+      this.AddDialogVisible = true
+    },
+    handleChange() {},
+    resetForm() {
+      this.$refs.addressFormRef.resetFields()
+    },
+    showProDia() {
+      //   const { data: res } = await this.$http.get('kuaidi/804909574412544580')
+      //   if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      //   this.progressInfo = res.data
+      //   console.log(res.data)
+      this.ProgressDialogVisible = true
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import '../../plugins/timeline/timeline.css';
+@import '../../plugins/timeline-item/timeline-item.css';
+.el-cascader {
+  width: 100%;
+}
+</style>
